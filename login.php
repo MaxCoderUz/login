49a61160847b3006dfebdb8e7f1c81438716a9ae
Elbek Khamdullaev <elbek.khamdullaev@mail.ru>
2021-06-20 19:41:26 +0500

    files add

--- login.php
+++ login.php
@@ -1,38 +1,90 @@
-<?php
-
-?>
+<?php ?>
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/style.css">
-</head>
+	<title>Login</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
+	<link rel="icon" type="image/png" href="../images/icons/favicon.ico"/>
+
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
+	
+	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
+
+	<link rel="stylesheet" type="text/css" href="../css/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+
+</head>
 <body>
-    <div class="container">
-        <div class="login">
-            <div class="row">
-                <h2>Kirish</h2>
-                <div class="col-md-12">
-                    <form action="validate.php" method="post">
-                    <label for="login">Login</label>
-                    <input name="login" type="text" class="form-control" id="login" placeholder="admin">
-                    <br>
-                    <label for="password">Parol</label>
-                    <input name="password" type="password" class="form-control" id="password" placeholder="1234">
-                    <input type="submit" id="login" value="Login" class="btn btn-outline-success btn-form">
-                    </form>                    
-                </div>
-            </div>
-        </div>
-    </div>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="../images/img-01.png" alt="IMG">
+				</div>
+
+				<form action="validate.php" method="post" class="login100-form validate-form">
+					<span class="login100-form-title">
+						Login
+					</span>
+					<div class="wrap-input100 validate-input" data-validate = "Login is required">
+						<input class="input100" type="text" name="login" placeholder="login">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-12">
+						
+					</div>
+
+					<div class="text-center p-t-136">
+						
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+
+	<script src="../vendor/bootstrap/js/popper.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+
+	<script src="../vendor/select2/select2.min.js"></script>
+
+	<script src="../vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+	<script src="../js/main.js"></script>
+
 </body>
 </html>
-
